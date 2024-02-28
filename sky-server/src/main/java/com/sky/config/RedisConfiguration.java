881ebfb3ce810587486cf70af83492765f867be5
youthOfMe <2040690180@qq.com>
2024-02-28 21:30:01 +0800

    feat: 准备测试整体项目

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -1,16 +1,19 @@
 package com.sky.config;
 
-import jdk.jpackage.internal.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Slf4j
+@Configuration
 public class RedisConfiguration {
 
     @Bean
     public RedisTemplate redisTemplate (RedisConnectionFactory redisConnectionFactory) {
-        Log.info("开始创建redis模板对象...");
+        log.info("开始创建redis模板对象...");
         RedisTemplate redisTemplate = new RedisTemplate();
         // 设置redis的连接工厂对象
         redisTemplate.setConnectionFactory(redisConnectionFactory);
