3d9e201ab5900bd020666017541fe496cb273cc6
youthOfMe <2040690180@qq.com>
2024-03-24 16:50:07 +0800

    feat: 完善查询小程序查询分类接口

---
+++ sky-server/src/main/java/com/sky/controller/user/CategroyController.java
@@ -0,0 +1,30 @@
+package com.sky.controller.user;
+
+import com.sky.entity.Category;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("userCategoryController")
+@RequestMapping("/user/category")
+@Api(tags = "C端-分类接口")
+public class CategroyController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 查询分类
+     * @param type
+     * @return
+     */
+    public Result<List<Category>> list(Integer type) {
+        List<Category> list = categoryService.list(type);
+        return Result.success(list);
+    }
+}
