e8c02d93cfa035a531b3c15ba84a21d13df57b18
youthOfMe <2040690180@qq.com>
2024-04-01 22:47:29 +0800

    feat: 新增商家端各个状态的订单数量统计

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -43,4 +43,12 @@ public interface OrderMapper {
      */
     @Select("select * from orders where id=#{id}")
     Orders getById(Long id);
+
+    /**
+     * 根据状态统计订单数量
+     * @param toBeConfirmed
+     * @return
+     */
+    @Select("select count(id) from  orders where status = #{status}")
+    Integer countStatus(Integer toBeConfirmed);
 }
