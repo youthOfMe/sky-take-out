a7d0c232c503973c1f1f3295388ac3657746af63
youthOfMe <2040690180@qq.com>
2024-04-02 23:34:18 +0800

    feat: 新增商家端工作台根据时间段统计营业数据

---
+++ sky-server/src/main/java/com/sky/controller/admin/WorkSpaceController.java
@@ -0,0 +1,40 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.service.WorkspaceSevice;
+import com.sky.vo.BusinessDataVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * 工作台
+ */
+@RestController
+@RequestMapping("/admin/workspace")
+@Slf4j
+@Api(tags = "工作台相关接口")
+public class WorkSpaceController {
+
+    @Autowired
+    private WorkspaceSevice workspaceSevice;
+
+    @GetMapping("/businessData")
+    @ApiOperation("工作台今日数据查询")
+    public Result<BusinessDataVO> businessData() {
+        // 获得当天的开始时间
+        LocalDateTime begin = LocalDateTime.now().with(LocalTime.MIN);
+        // 获取当天结束时间
+        LocalDateTime end = LocalDateTime.now().with(LocalTime.MAX);
+
+        BusinessDataVO businessDataVO = workspaceSevice.getBusinessData(begin, end);
+        return Result.success(businessDataVO);
+    }
+}
