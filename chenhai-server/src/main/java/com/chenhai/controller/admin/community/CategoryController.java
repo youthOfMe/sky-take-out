7e36f2d42ac1752234f51501a04556c480e3d143
youthOfMe <2040690180@qq.com>
2024-04-23 23:49:24 +0800

    feat: 完善管理端-社区一级分类管理接口

---
+++ chenhai-server/src/main/java/com/chenhai/controller/admin/community/CategoryController.java
@@ -0,0 +1,36 @@
+package com.chenhai.controller.admin.community;
+
+import com.chenhai.entity.community.CommunityParentCategory;
+import com.chenhai.result.Result;
+import com.chenhai.service.community.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("adminCommunityCategoryController")
+@RequestMapping("/admin/community/category")
+@Api(tags = "管理端管理社区分类相关接口")
+@Slf4j
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 一级分类板块查询
+     * @return
+     */
+    @GetMapping("/parentList")
+    @ApiOperation("一级分类板块查询")
+    public Result<List<CommunityParentCategory>> parentCategoryList() {
+        log.info("用户查询一级分类");
+        List<CommunityParentCategory> list = categoryService.parentCategory();
+        return Result.success(list);
+    }
+}
